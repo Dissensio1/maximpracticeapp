7d6e92b31c6cc11f518964db2fc86400c0d9a1ff
Emil Zotov <emil.zotov2004@gmail.com>
2025-11-27 12:01:28 +0300

    Preparing for Benchmark -Refactoring and fixes

--- Program.cs
+++ Program.cs
@@ -1,103 +1,4 @@
 ﻿namespace Program;
-public static class IncrementalAlgorithm
-{
-    public static List<Driver> FindNearestPoints(Order order, Driver[] drivers, int k = 5)
-    {
-        if (drivers == null || drivers.Length == 0)
-            return new List<Driver>();
-        
-        k = Math.Min(k, drivers.Length);
-
-        var nearestDrivers = new List<(double distance, Driver driver)>();
-        double maxDistance = double.MaxValue;
-
-        foreach (var driver in drivers)
-        {
-            double dist = Math.Pow(driver.XCord - order.XCord, 2) + Math.Pow(driver.YCord - order.YCord, 2);
-            
-            if (nearestDrivers.Count < k)
-            {
-                // Если еще не набрали k точек, просто добавляем
-                nearestDrivers.Add((dist, driver));
-                if (nearestDrivers.Count == k)
-                {
-                    // Находим текущее максимальное расстояние
-                    maxDistance = nearestDrivers.Max(x => x.distance);
-                }
-            }
-            else
-            {
-                // Если нашли точку ближе, чем самая дальняя в текущем списке
-                if (dist < maxDistance)
-                {
-                    // Находим индекс самой дальней точки
-                    int maxIndex = -1;
-                    double currentMax = double.MinValue;
-                    
-                    for (int i = 0; i < nearestDrivers.Count; i++)
-                    {
-                        if (nearestDrivers[i].distance > currentMax)
-                        {
-                            currentMax = nearestDrivers[i].distance;
-                            maxIndex = i;
-                        }
-                    }
-                    
-                    if (maxIndex >= 0)
-                    {
-                        // Удаляем самую дальнюю точку и добавляем новую
-                        nearestDrivers.RemoveAt(maxIndex);
-                        nearestDrivers.Add((dist, driver));
-                        
-                        // Обновляем максимальное расстояние
-                        maxDistance = nearestDrivers.Max(x => x.distance);
-                    }
-                }
-            }
-        }
-        return nearestDrivers.OrderBy(x => x.distance).Select(x => x.driver).ToList();
-    }
-}
-public static class HeapAlgorithm
-{
-    public static List<Driver> FindNearestPoints(Order order, Driver[] drivers, int k = 5)
-    {
-        if (drivers == null || drivers.Length == 0)
-            return new List<Driver>();
-        
-        k = Math.Min(k, drivers.Length);
-
-        // Используем SortedSet как max-heap (самая дальняя точка будет первой)
-        var maxHeap = new SortedSet<(double distance, Driver driver)>(Comparer<(double distance, Driver driver)>.Create((a, b) => 
-        {
-            int cmp = b.distance.CompareTo(a.distance); // Обратный порядок для max-heap
-            if (cmp == 0) return a.driver.GetHashCode().CompareTo(b.driver.GetHashCode());
-            return cmp;
-        }));
-
-        foreach (var driver in drivers)
-        {
-            double dist = Math.Pow(driver.XCord - order.XCord, 2) + Math.Pow(driver.YCord - order.YCord, 2);
-            
-            if (maxHeap.Count < k)
-            {
-                maxHeap.Add((dist, driver));
-            }
-            else
-            {
-                // Если текущая точка ближе, чем самая дальняя в куче
-                var farthest = maxHeap.First();
-                if (dist < farthest.distance)
-                {
-                    maxHeap.Remove(farthest);
-                    maxHeap.Add((dist, driver));
-                }
-            }
-        }
-
-        return maxHeap.Select(x => x.driver).ToList();
-    }
-}
 internal static class Program
 {
     private static void Main(string[] args)
@@ -110,8 +11,14 @@ internal static class Program
         int n = int.Parse(Console.ReadLine());
         Console.Write("Введите m: ");
         int m = int.Parse(Console.ReadLine());
+        Console.Write("Введите количество водителей: ");
+        int size = int.Parse(Console.ReadLine());
 
-        int size = 10;
+        if(size > m * n)
+        {
+            size = rand.Next(1, m + n - 1);
+        }
+
         Driver[] driversArr;
         Order newOrder;
         var uniqueDrivers = new HashSet<Driver>();
@@ -151,15 +58,16 @@ internal static class Program
             Console.WriteLine(" distance:" + driversArr[i].Distance);
         }
 
-        Array.Sort(driversArr, (d1, d2) => d1.Distance.CompareTo(d2.Distance));
+        var nearest = Map.FindNearestDriversSort(newOrder, driversArr, 5);
+
         Console.WriteLine("Алгоритм 1 (сортировка):");
-        for(int i = 0; i < 5; i++)
+        for(int i = 0; i < nearest.Count; i++)
         {
-            Console.Write($"id: {driversArr[i].Identifier}");
-            Console.WriteLine($" distance: {driversArr[i].Distance}");
+            Console.Write($"id: {nearest[i].Identifier}");
+            Console.WriteLine($" distance: {nearest[i].Distance}");
         }
 
-        var nearest = HeapAlgorithm.FindNearestPoints(newOrder, driversArr, 5);
+        nearest = Map.FindNearestDriversHeap(newOrder, driversArr, 5);
         
         Console.WriteLine("Алгоритм 2 (куча):");
         for (int i = 0; i < nearest.Count; i++)
@@ -168,9 +76,9 @@ internal static class Program
             Console.WriteLine($" distance: {nearest[i].Distance}");
         }
 
-        nearest = IncrementalAlgorithm.FindNearestPoints(newOrder, driversArr, 5);
+        nearest = Map.FindNearestDriversIncr(newOrder, driversArr, 5);
         
-        Console.WriteLine("Алгоритм 4 (инкрементальный):");
+        Console.WriteLine("Алгоритм 3 (инкрементальный):");
         for (int i = 0; i < nearest.Count; i++)
         {
             Console.Write($"id: {nearest[i].Identifier}");
